8ac8d81add3dc7fb60e1a93e9f9a32db2fb7b0e5
oneKOhit <a.thyholt.henriksen@outlook.com>
2017-03-21 14:36:29 +0100

    done

---
+++ project/Client/Client.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+import socket
+import json
+import sys
+import codecs
+from MessageReceiver import MessageReceiver
+from MessageParser import MessageParser
+
+class Client:
+    def __init__(self, host, server_port):
+        self.host = host
+        self.server_port = server_port
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.run()
+
+    def run(self):
+        self.connection.connect((self.host, self.server_port))
+        self.thread = MessageReceiver(self, self.connection)
+        self.thread.start()
+        
+        while True:
+            text = input().split(' ', 1)
+            if(len(text) > 0 and len(text) < 3):
+                if(len(text)==1):
+                    text.append('')
+                if(text[0] == 'login'):
+                    payload=json.dumps({'request': 'login', 'content': text[1]})
+                    self.send_payload(payload)
+                elif(text[0] == 'logout'):
+                    self.disconnect()
+                elif(text[0] == 'names'):
+                    payload=json.dumps({'request': 'names'})
+                    self.send_payload(payload)
+                elif(text[0] == 'help'):
+                    payload=json.dumps({'request': 'help'})
+                    self.send_payload(payload)
+                elif(text[0] == 'msg'):
+                    payload=json.dumps({'request': 'msg', 'content': text[1]})
+                    self.send_payload(payload)
+                else:
+                    print('Unknown command, type "help" for help \n >', end='')
+            else:
+                print('Unknown command, type "help" for help \n >', end='')
+
+
+    def disconnect(self):
+        payload=json.dumps({'request': 'logout'})
+        self.send_payload(payload)
+        sys.exit()
+
+    def receive_message(self, message):
+        parser = MessageParser()
+        parsed_message = parser.parse(message)
+        print(parsed_message,'\n> ',end='')
+
+    def send_payload(self, data):
+        self.connection.send(bytes(data, 'UTF-8'))
+
+
+if __name__ == '__main__':
+    """
+    This is the main method and is executed when you type "python Client.py"
+    in your terminal.
+    No alterations is necessary
+    """
+    connect_ip=input("Enter server ip to connect: ")
+    print('Type "login <username>" to log in')
+    client = Client(connect_ip, 9998)
---
+++ project/Client/MessageParser.py
@@ -0,0 +1,34 @@
+import json
+from MessageReceiver import MessageReceiver
+
+
+class MessageParser():
+    def __init__(self):
+        self.possible_responses = {
+            'error': self.parse_error,
+            'info': self.parse_info,
+            'msg': self.parse_message,
+            'history': self.parse_history
+        }
+
+    def parse(self, payload):
+        if payload['response'] in self.possible_responses:
+            return self.possible_responses[payload['response']](payload)
+        else:
+            return "Response not valid"
+
+    def parse_error(self, payload):
+        return "[Error]" + payload['content']
+
+    def parse_info(self, payload):
+        return "[Info] " + payload['content']
+
+    def parse_message(self, payload):
+        return "[" + str(payload["timestamp"]) + " - " + str(payload['sender']) + "] " + str(payload["content"]) +"\n"
+
+    def parse_history(self, payload):
+        history = "[History] \n"
+        for message in payload['content']:
+            parser = MessageParser()
+            history += parser.parse(message) + "\n"
+        return history
